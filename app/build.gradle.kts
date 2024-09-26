420a229279f067b3f2301ca67161ebcd3257e4a6
Sergio Sánchez <sss4esob@gmail.com>
2024-09-26 22:27:22 +0200

    Update to version 0.0.39

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ publishing {
             run {
                 groupId = "com.dreamsoftware.libraries"
                 artifactId = "fudge-tv-compose"
-                version = "0.0.38"
+                version = "0.0.39"
                 artifact("$buildDir/outputs/aar/app-release.aar")
             }
         }
